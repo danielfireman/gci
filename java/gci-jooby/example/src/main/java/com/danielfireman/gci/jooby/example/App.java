121984ed1b2646c138caf5a8ae985e8269f0a9e7
Daniel Fireman <danielfireman@gmail.com>
2017-01-17 14:03:10 -0300

    Commiting application example

---
+++ java/gci-jooby/example/src/main/java/com/danielfireman/gci/jooby/example/App.java
@@ -0,0 +1,23 @@
+package com.danielfireman.gci.jooby.example;
+
+import com.danielfireman.gci.jooby.JoobyGciFilter;
+import org.jooby.Jooby;
+
+/**
+ * Example of application that uses {@code JoobyGciFilter}.
+ *
+ * @author danielfireman
+ * @see JoobyGciFilter
+ */
+public class App extends Jooby {
+
+    {
+        use("GET", "*", new JoobyGciFilter());
+        get("/", () -> "Hello World!");
+    }
+
+    public static void main(final String[] args) {
+        run(App::new, args);
+    }
+
+}
